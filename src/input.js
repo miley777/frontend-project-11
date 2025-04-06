ae99dda7daa25ae1342cafa45a42ef8c06ab13dc
Maria Berina <gia0sam8cint@bk.ru>
2025-04-06 11:31:13 +0300

    Update ./src/index.html, ./src/input.js, ./src/js/main.js

--- src/input.js
+++ src/input.js
@@ -1,9 +1,9 @@
 import * as yup from 'yup';
 
 export default () => {
-    //console.log('gjkhg');
+    console.log('gjkhg');
     const form = document.querySelector('div.row');
-    form.addEventListener('submit', async (e) => {
+    form.addEventListener('submit', (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const list = document.createElement('ul');
@@ -11,21 +11,24 @@ export default () => {
         const ent = [...formData.entries()];
         ent.forEach((ert) => {
             const [key, value] = ert;
-            const valueEsc = htmlEscape(value);
-            let linkSchema = yup.string().url().lowercase().trim().nullable().test({
-                name: 'is-link',
-                test(value, ctx) {
-                    if (!value.startsWith('http')) {
-                        return ctx.createError({ message: 'LINK missing correct prefix' });
-                    }
-                    if (!value.length < 12) {
-                        ctx.createError({ message: 'LINK is not the right length' });
-                    }
-                    return true;
-                },
-            });
-            let link = await linkSchema.validate(valueEsc);
-            //const linkJson = await link.json();
+            let linkSchema = yup.string().url().lowercase().trim().nullable().test('checkLinkUnique',
+                value => fetch(link).then(async res => {
+                    const { isLinkUnique } = await res.json()
+                    return isLinkUnique
+                }),
+                //name: 'is-link',
+                //test(value, ctx) {
+                  //  if (!value.startsWith('http')) {
+                    //    return ctx.createError({ message: 'LINK missing correct prefix' });
+                    //}
+                    //if (!value.length < 12) {
+                      //  ctx.createError({ message: 'LINK is not the right length' });
+                    //}
+                    //return true;
+                //},
+            );
+            let link = linkSchema.validate(value) ? value : 'nothing';
+            //const { linkJson } = await link.json();
             list.innerHTML  = `<li>${link}</li>`;
         });
         dataDiv.append(list);
